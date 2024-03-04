b69f062b95ea866f2a617b1975a6a865f417c13c
Mac Tirney <mactirney@gmail.com>
2024-03-04 12:49:42 -0500

    feat: add StopProgress Lua function examples
    Establish a 'StopProgress' function folder to contain StopProgress Lua function examples.
    Include StopProgress Lua function examples in the 'StopProgress' folder.

---
+++ modules/Object Free API Functions/Functions/Interface Functions/Progress Bar Functions/S/StopProgress()/stopProgressFuncExample.lua
@@ -0,0 +1,10 @@
+--* StopProgress() Function - Example
+
+--* About
+    -- This example stops the progress bar created using the StartProgress example.
+
+--* Example 1
+return function()
+    -- Stops and closes the progress bar with the matching handle
+    StopProgress(progressHandle)
+end
