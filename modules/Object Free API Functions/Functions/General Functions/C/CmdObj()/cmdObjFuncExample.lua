1c0d7a32a0251bda44815f11dd98876a5a6dc455
Mac Tirney <mactirney@gmail.com>
2024-03-05 16:00:08 -0500

    feat: add CmdObj Lua function examples
    Establish a 'CmdObj' function folder to contain CmdObj Lua function examples.
    Include CmdObj Lua function examples in the 'CmdObj' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/C/CmdObj()/cmdObjFuncExample.lua
@@ -0,0 +1,14 @@
+--* CmdObj() Function - Example
+
+--* About
+    -- This example uses the Dump function on the command object. 
+        -- It lists all the properties and lists the children.
+
+--* Example 1
+local function main()
+    
+    CmdObj():Dump()
+    
+end
+
+return main
