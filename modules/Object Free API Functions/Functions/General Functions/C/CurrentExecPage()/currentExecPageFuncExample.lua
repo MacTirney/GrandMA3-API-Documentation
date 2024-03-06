c7c4bb2302d2d5d0d900750bdcd99372b07e5d75
Mac Tirney <mactirney@gmail.com>
2024-03-05 19:05:59 -0500

    feat: add CurrentExecPage Lua function examples
    Establish a 'CurrentExecPage' function folder to contain CurrentExecPage Lua function examples.
    Include CurrentExecPage Lua function examples in the 'CurrentExecPage' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/C/CurrentExecPage()/currentExecPageFuncExample.lua
@@ -0,0 +1,13 @@
+--* CurrentExecPage() Function - Example
+
+--* About
+    -- This example prints the data connected to the handle.
+        -- It uses the Dump() function.
+
+--* Example 1
+return function()
+
+    -- Dumps information about the current executor page
+    CurrentExecPage():Dump()
+    
+end
