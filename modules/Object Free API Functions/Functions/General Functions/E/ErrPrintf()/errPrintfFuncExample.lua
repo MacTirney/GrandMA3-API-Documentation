55b86f2991a0b10bdee5163036f8beeffc7d840e
Mac Tirney <mactirney@gmail.com>
2024-03-06 11:50:55 -0500

    feat: add ErrPrintf Lua function examples
    Establish a 'ErrPrintf' function folder to contain ErrPrintf Lua function examples.
    Include ErrPrintf Lua function examples in the 'ErrPrintf' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/E/ErrPrintf()/errPrintfFuncExample.lua
@@ -0,0 +1,13 @@
+--* ErrPrintf() Function - Example
+
+--* About
+    -- This example prints "This is a red error message!" in the Command Line History and System Monitor.
+
+--* Example 1
+local function main()
+
+    ErrPrintf("This is a red error message!")
+    
+end
+
+return main
