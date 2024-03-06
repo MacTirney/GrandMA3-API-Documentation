32950db6ddb4e848bb8dc9645d37215d4f7be845
Mac Tirney <mactirney@gmail.com>
2024-03-06 11:16:15 -0500

    feat: add DeskLocked Lua function examples
    Establish a 'DeskLocked' function folder to contain DeskLocked Lua function examples.
    Include DeskLocked Lua function examples in the 'DeskLocked' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/D/DeskLocked()/deskLockedFuncExample.lua
@@ -0,0 +1,14 @@
+--* DeskLocked() Function - Example
+
+--* About
+    -- This example prints the boolean number indicating the "DeskLocked" status to the Command Line History.
+
+--* Example 1
+local function main() 
+
+    -- The DeskLocked() return is printed.
+    Printf(DeskLocked())
+    
+end
+
+return main
