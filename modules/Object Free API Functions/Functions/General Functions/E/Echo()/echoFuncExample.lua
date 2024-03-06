2cdb56cc13f37bd2d1115d813a338da3b93b6ab6
Mac Tirney <mactirney@gmail.com>
2024-03-06 11:48:32 -0500

    feat: add Echo Lua function examples
    Establish a 'Echo' function folder to contain Echo Lua function examples.
    Include Echo Lua function examples in the 'Echo' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/E/Echo()/echoFuncExample.lua
@@ -0,0 +1,13 @@
+--* Echo() Function - Example
+
+--* About
+    -- This example prints "Hello World!" on the System Monitor.
+
+--* Example 1
+local function main()
+
+    Echo("Hello World!")
+
+end
+
+return main
