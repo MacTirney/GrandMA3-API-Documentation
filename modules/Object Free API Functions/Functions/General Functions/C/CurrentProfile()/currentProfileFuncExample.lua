b2e2877ce8a5ffd1062bb796ea8cb60644ecb727
Mac Tirney <mactirney@gmail.com>
2024-03-05 19:07:27 -0500

    feat: add CurrentProfile Lua function examples
    Establish a 'CurrentProfile' function folder to contain CurrentProfile Lua function examples.
    Include CurrentProfile Lua function examples in the 'CurrentProfile' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/C/CurrentProfile()/currentProfileFuncExample.lua
@@ -0,0 +1,13 @@
+--* CurrentProfile() Function - Example
+
+--* About
+    -- This example prints the data connected to the handle.
+        -- It uses the Dump() function.
+
+--* Example 1
+return function()
+
+    -- Dumps information about the current executor page
+    CurrentProfile():Dump()
+    
+end
