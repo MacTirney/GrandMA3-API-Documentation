b02852d7e5a22bddd41a46b5c2ad097674e2b805
Mac Tirney <mactirney@gmail.com>
2024-03-05 19:04:37 -0500

    feat: add CurrentEnvironment Lua function examples
    Establish a 'CurrentEnvironment' function folder to contain CurrentEnvironment Lua function examples.
    Include CurrentEnvironment Lua function examples in the 'CurrentEnvironment' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/C/CurrentEnvironment()/currentEnvironmentFuncExample.lua
@@ -0,0 +1,13 @@
+--* CurrentEnvironment() Function - Example
+
+--* About
+    -- This example prints the data connected to the handle. 
+        -- It uses the Dump() function.
+
+--* Example 1
+return function()
+
+    -- Dumps information about the current environment
+    CurrentEnvironment():Dump()
+    
+end
