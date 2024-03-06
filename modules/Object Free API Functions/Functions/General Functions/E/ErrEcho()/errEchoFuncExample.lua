9f17d6933f5223e483262df6d742797134354170
Mac Tirney <mactirney@gmail.com>
2024-03-06 11:49:36 -0500

    feat: add ErrEcho Lua function examples
    Establish a 'ErrEcho' function folder to contain ErrEcho Lua function examples.
    Include ErrEcho Lua function examples in the 'ErrEcho' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/E/ErrEcho()/errEchoFuncExample.lua
@@ -0,0 +1,13 @@
+--* ErrEcho() Function - Example
+
+--* About
+    -- This prints "This is a red error message!" on the system monitor.
+
+--* Example 1
+local function main()
+
+    ErrEcho("This is a red error message!")
+
+end
+
+return main
