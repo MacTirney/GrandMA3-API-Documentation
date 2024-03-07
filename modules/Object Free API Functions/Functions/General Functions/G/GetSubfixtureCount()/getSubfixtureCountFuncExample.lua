36b672d48f19fa131caff069e231ddf6df8fe2ee
Mac Tirney <mactirney@gmail.com>
2024-03-07 14:28:11 -0500

    feat: add GetSubfixtureCount Lua function examples
    Establish a 'GetSubfixtureCount' function folder to contain GetSubfixtureCount Lua function examples.
    Include GetSubfixtureCount Lua function examples in the 'GetSubfixtureCount' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/G/GetSubfixtureCount()/getSubfixtureCountFuncExample.lua
@@ -0,0 +1,11 @@
+--* GetSubfixtureCount() Function - Example
+
+--* About
+    -- This example prints the total number of patched fixtures in the Command Line History.
+
+--* Example 1
+return function ()
+
+    Printf('Total number of patched fixtures: %i', GetSubfixtureCount())
+    
+end
