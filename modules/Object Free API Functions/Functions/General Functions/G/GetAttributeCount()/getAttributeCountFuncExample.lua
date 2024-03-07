0b7742372ec4aaebb3c1176deea6f8898050399d
Mac Tirney <mactirney@gmail.com>
2024-03-07 13:09:02 -0500

    feat: add GetAttributeCount Lua function examples
    Establish a 'GetAttributeCount' function folder to contain GetAttributeCount Lua function examples.
    Include GetAttributeCount Lua function examples in the 'GetAttributeCount' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/G/GetAttributeCount()/getAttributeCountFuncExample.lua
@@ -0,0 +1,11 @@
+--* GetAttributeCount() Function - Example
+
+--* About
+    -- This example prints the returned number in the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf("The attribute count is %i", GetAttributeCount())
+    
+end
