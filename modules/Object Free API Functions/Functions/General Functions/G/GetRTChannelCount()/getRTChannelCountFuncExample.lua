18c18444a952fd454f6b37112c139c9c8bd2e641
Mac Tirney <mactirney@gmail.com>
2024-03-07 14:20:42 -0500

    feat: add GetRTChannelCount Lua function examples
    Establish a 'GetRTChannelCount' function folder to contain GetRTChannelCount Lua function examples.
    Include GetRTChannelCount Lua function examples in the 'GetRTChannelCount' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/G/GetRTChannelCount()/getRTChannelCountFuncExample.lua
@@ -0,0 +1,11 @@
+--* GetRTChannelCount() Function - Example
+
+--* About
+    -- This example prints the number of RT channels to the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf("The number of RT channels is " .. GetRTChannelCount())
+    
+end
