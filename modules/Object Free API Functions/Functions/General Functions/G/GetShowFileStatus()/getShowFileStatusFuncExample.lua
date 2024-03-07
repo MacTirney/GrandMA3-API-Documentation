9ff35a2c1f480a309aa7639acfafb3085b3d09b9
Mac Tirney <mactirney@gmail.com>
2024-03-07 14:25:40 -0500

    feat: add GetShowFileStatus Lua function examples
    Establish a 'GetShowFileStatus' function folder to contain GetShowFileStatus Lua function examples.
    Include GetShowFileStatus Lua function examples in the 'GetShowFileStatus' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/G/GetShowFileStatus()/getShowFileStatusFuncExample.lua
@@ -0,0 +1,13 @@
+--* GetShowFileStatus() Function - Example
+
+--* About
+    -- This example prints the current device's show file status in the Command Line History.
+
+--* Example 1
+local function main()
+
+    Printf("ShowfileStatus: "..GetShowFileStatus())
+    
+end
+
+return main
