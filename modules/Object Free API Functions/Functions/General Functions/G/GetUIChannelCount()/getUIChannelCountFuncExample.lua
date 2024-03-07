0e7c9c58ff025846cc8520693a8b57c11bc9a8eb
Mac Tirney <mactirney@gmail.com>
2024-03-07 14:32:11 -0500

    feat: add GetUIChannelCount Lua function examples
    Establish a 'GetUIChannelCount' function folder to contain GetUIChannelCount Lua function examples.
    Include GetUIChannelCount Lua function examples in the 'GetUIChannelCount' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/G/GetUIChannelCount()/getUIChannelCountFuncExample.lua
@@ -0,0 +1,11 @@
+--* GetUIChannelCount() Function - Example
+
+--* About
+    -- This example prints the amount of UI channels to the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf("The number of UI channels is " .. GetUIChannelCount())
+    
+end
