40fc8306ce064afa54a4134289ff83286ff9c029
Mac Tirney <mactirney@gmail.com>
2024-03-08 12:20:57 -0500

    feat: add HostType Lua function examples
    Establish a 'HostType' function folder to contain HostType Lua function examples.
    Include HostType Lua function examples in the 'HostType' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/H/HostType()/hostTypeFuncExample.lua
@@ -0,0 +1,13 @@
+--* HostType() Function - Example
+
+--* About
+    -- This example prints the host type of the device in the Command Line History.
+
+--* Example 1
+local function main()
+
+    Printf("HostType: "..HostType())
+    
+end
+
+return main
