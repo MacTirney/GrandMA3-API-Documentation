17b1143d42bd7f9394674d8c2a4751db351f0636
Mac Tirney <mactirney@gmail.com>
2024-03-08 15:00:51 -0500

    feat: add KeyboardObj Lua function examples
    Establish a 'KeyboardObj' function folder to contain KeyboardObj Lua function examples.
    Include KeyboardObj Lua function examples in the 'KeyboardObj' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/K/KeyboardObj()/keyboardObjFuncExample.lua
@@ -0,0 +1,16 @@
+--* KeyboardObj() Function - Example
+
+--* About
+    -- This example prints the information of the keyboard object.
+
+--* Example 1
+return function()
+
+    -- Print all informatin about the KeyboardObj object
+    Printf("=============== START OF DUMP ===============")
+
+    KeyboardObj():Dump()
+    
+    Printf("================ END OF DUMP ================")
+
+end
