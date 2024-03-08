1f67456a8fe6c4e1531ddb899a1e1a979142b6bd
Mac Tirney <mactirney@gmail.com>
2024-03-08 15:04:40 -0500

    feat: add MouseObj Lua function examples
    Establish a 'MouseObj' function folder to contain MouseObj Lua function examples.
    Include MouseObj Lua function examples in the 'MouseObj' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/M/MouseObj()/mouseObjFuncExample.lua
@@ -0,0 +1,16 @@
+--* MouseObj() Function - Example
+
+--* About
+    -- This example prints the information of the mouse object.
+
+--* Example 1
+return function()
+
+    -- Print all informatin about the MouseObj object
+    Printf("=============== START OF DUMP ===============")
+
+    MouseObj():Dump()
+
+    Printf("================ END OF DUMP ================")
+
+end
