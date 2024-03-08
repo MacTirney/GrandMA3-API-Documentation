5aac749eecaf1cb05d7a4285c9c07919b7279064
Mac Tirney <mactirney@gmail.com>
2024-03-08 11:43:57 -0500

    feat: add HostSubType Lua function examples
    Establish a 'HostSubType' function folder to contain HostSubType Lua function examples.
    Include HostSubType Lua function examples in the 'HostSubType' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/H/HostSubType()/hostSubTypeFuncExample.lua
@@ -0,0 +1,13 @@
+--* HostSubType() Function - Example
+
+--* About
+    -- This example prints the host sub-type of the station in the Command Line History.
+
+--* Example 1
+local function main()
+
+    Printf("HostSubType: " .. HostSubType())
+    
+end
+
+return main
