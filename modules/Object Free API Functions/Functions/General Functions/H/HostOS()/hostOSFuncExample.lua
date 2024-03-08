8de3c98ce6d4d11bfe1cc53a5c8510ad74b1b825
Mac Tirney <mactirney@gmail.com>
2024-03-08 11:42:58 -0500

    feat: add HostOS Lua function examples
    Establish a 'HostOS' function folder to contain HostOS Lua function examples.
    Include HostOS Lua function examples in the 'HostOS' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/H/HostOS()/hostOSFuncExample.lua
@@ -0,0 +1,13 @@
+--* HostOS() Function - Example
+
+--* About
+    -- This example prints the operating system of the device in the Command Line History.
+
+--* Example 1
+local function main()
+
+    Printf("HostOS: " .. HostOS())
+
+end
+
+return main
