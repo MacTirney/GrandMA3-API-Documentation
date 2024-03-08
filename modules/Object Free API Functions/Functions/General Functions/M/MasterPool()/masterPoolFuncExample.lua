27a03bc99e778a22dcc0a9fb988e5235e2676b94
Mac Tirney <mactirney@gmail.com>
2024-03-08 15:02:07 -0500

    feat: add MasterPool Lua function examples
    Establish a 'MasterPool' function folder to contain MasterPool Lua function examples.
    Include MasterPool Lua function examples in the 'MasterPool' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/M/MasterPool()/masterPoolFuncExample.lua
@@ -0,0 +1,16 @@
+--* MasterPool() Function - Example
+
+--* About
+    -- This example prints the information of the MasterPool object.
+
+--* Example 1
+return function()
+
+    -- Print all informatin about the MasterPool object
+    Printf("=============== START OF DUMP ===============")
+
+    MasterPool():Dump()
+
+    Printf("================ END OF DUMP ================")
+    
+end
