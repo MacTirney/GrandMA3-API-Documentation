3b76f2e465e3ccf0ad18d83a1c5ad80029490a5e
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:47:02 -0400

    feat: add SerialNumber Lua function examples
    Establish a 'SerialNumber' function folder to contain SerialNumber Lua function examples.
    Include SerialNumber Lua function examples in the 'SerialNumber' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/SerialNumber()/serialNumberFuncExample.lua
@@ -0,0 +1,11 @@
+--* SerialNumber() Function - Example
+
+--* About
+    -- This example prints the serial number in the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf("Serial number: " .. SerialNumber())
+    
+end
