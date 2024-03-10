d580bb606c33a743efa5f0ea0ef1c25acae15936
Mac Tirney <mactirney@gmail.com>
2024-03-09 21:50:09 -0500

    feat: add Printf Lua function examples
    Establish a 'Printf' function folder to contain Printf Lua function examples.
    Include Printf Lua function examples in the 'Printf' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/P/Printf()/printfFuncExample.lua
@@ -0,0 +1,13 @@
+--* Printf() Function - Example
+
+--* About
+    -- This example prints "Hello World!" in the Command Line History.
+
+--* Example 1
+local function main()
+
+    Printf("Hello World!")
+    
+end
+
+return main
