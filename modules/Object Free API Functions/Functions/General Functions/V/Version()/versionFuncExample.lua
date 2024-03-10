57cd2240f7dd1547faf79c171cb800cccb07ea03
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:57:17 -0400

    feat: add Version Lua function examples
    Establish a 'Version' function folder to contain Version Lua function examples.
    Include Version Lua function examples in the 'Version' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/V/Version()/versionFuncExample.lua
@@ -0,0 +1,13 @@
+--* Version() Function - Example
+
+--* About
+    -- This example prints the software version in the Command Line History:
+
+--* Example 1
+local function main()
+
+    Printf("Software version: " .. Version())
+    
+end
+
+return main
