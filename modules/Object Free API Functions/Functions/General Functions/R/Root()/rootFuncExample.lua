50ee202619405bfd1463f4daef7b4d8edc71ac31
Mac Tirney <mactirney@gmail.com>
2024-03-09 21:55:25 -0500

    feat: add Root Lua function examples
    Establish a 'Root' function folder to contain Root Lua function examples.
    Include Root Lua function examples in the 'Root' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/R/Root()/rootFuncExample.lua
@@ -0,0 +1,13 @@
+--* Root() Function - Example
+
+--* About
+    -- This simple example prints the information of the Root object in the Command Line History using the Dump() function.
+
+--* Example 1
+local function main()
+
+    Root():Dump()
+
+end
+
+return main
