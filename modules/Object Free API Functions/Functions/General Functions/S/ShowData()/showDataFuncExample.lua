a5cd5a3a8e211d10abc25203c0789d0ed78d58de
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:49:53 -0400

    feat: add ShowData Lua function examples
    Establish a 'ShowData' function folder to contain ShowData Lua function examples.
    Include ShowData Lua function examples in the 'ShowData' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/ShowData()/showDataFuncExample.lua
@@ -0,0 +1,13 @@
+--* ShowData() Function - Example
+
+--* About
+    -- This simple example prints the information of the ShowData object in the Command Line History using the Dump() function.
+
+--* Example 1
+local function main()
+
+    ShowData():Dump()
+    
+end
+
+return main
