84f469d092343fa024992acf28104a26038d076d
Mac Tirney <mactirney@gmail.com>
2024-03-09 21:53:49 -0500

    feat: add ReleaseType Lua function examples
    Establish a 'ReleaseType' function folder to contain ReleaseType Lua function examples.
    Include ReleaseType Lua function examples in the 'ReleaseType' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/R/ReleaseType()/releaseTypeFuncExample.lua
@@ -0,0 +1,11 @@
+--* ReleaseType() Function - Example
+
+--* About
+    -- This example prints the release type in the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf("The ReleaseType is "..ReleaseType())
+    
+end
