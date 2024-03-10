2c368be07c010a74e617358794c58673a281a29c
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:50:39 -0400

    feat: add ShowSettings Lua function examples
    Establish a 'ShowSettings' function folder to contain ShowSettings Lua function examples.
    Include ShowSettings Lua function examples in the 'ShowSettings' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/ShowSettings()/showSettingsFuncExample.lua
@@ -0,0 +1,13 @@
+--* ShowSettings() Function - Example
+
+--* About
+    -- This simple example prints the information of the ShowSettings object using the Dump function.
+
+--* Example 1
+local function main()
+
+    ShowSettings():Dump()
+    
+end
+
+return main
