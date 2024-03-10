b9fae7df763fcd56b85d7d4f17e05284f053354f
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:32:22 -0400

    feat: add SelectionCount Lua function examples
    Establish a 'SelectionCount' function folder to contain SelectionCount Lua function examples.
    Include SelectionCount Lua function examples in the 'SelectionCount' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/SelectionCount()/selectionCountFuncExample.lua
@@ -0,0 +1,11 @@
+--* SelectionCount() Function - Example
+
+--* About
+    -- This example prints the number of fixtures in the current selection to the Command Line History.
+
+--* Example 1
+return function()
+
+    Printf('Number of fixtures in the current selection: %i', SelectionCount())
+
+end
