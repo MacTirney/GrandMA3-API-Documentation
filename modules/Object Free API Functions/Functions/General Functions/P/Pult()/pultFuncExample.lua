bb1439d156e2e3b6090549b66e9394f3745a7492
Mac Tirney <mactirney@gmail.com>
2024-03-09 21:52:29 -0500

    feat: add Pult Lua function examples
    Establish a 'Pult' function folder to contain Pult Lua function examples.
    Include Pult Lua function examples in the 'Pult' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/P/Pult()/pultFuncExample.lua
@@ -0,0 +1,17 @@
+--* Pult() Function - Example
+
+--* About
+    -- This example prints the data connected to the handle.
+        -- It uses the Dump() function.
+
+--* Example 1
+return function()
+
+    -- The following prints the dump for the pult object
+    Printf("=============== START OF DUMP ===============")
+
+    Pult():Dump()
+
+    Printf("================ END OF DUMP ================")
+    
+end
