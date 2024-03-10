03237828fbe5daa003e0a38f8d672c18a6d53349
Mac Tirney <mactirney@gmail.com>
2024-03-09 21:51:16 -0500

    feat: add ProgrammerPart Lua function examples
    Establish a 'ProgrammerPart' function folder to contain ProgrammerPart Lua function examples.
    Include ProgrammerPart Lua function examples in the 'ProgrammerPart' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/P/ProgrammerPart()/programmerPartFuncExample.lua
@@ -0,0 +1,13 @@
+--* ProgrammerPart() Function - Example
+
+--* About
+    -- This simple example prints the information of the ProgrammerPart object.
+
+--* Example 1
+local function main()
+
+    ProgrammerPart():Dump()
+
+end
+  
+return main
