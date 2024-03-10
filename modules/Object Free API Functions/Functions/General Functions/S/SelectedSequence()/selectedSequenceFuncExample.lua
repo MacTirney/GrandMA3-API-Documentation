0f41a7520d3a007b47c9fef1dc95d0a37a4b8574
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:29:36 -0400

    feat: add SelectedSequence Lua function examples
    Establish a 'SelectedSequence' function folder to contain SelectedSequence Lua function examples.
    Include SelectedSequence Lua function examples in the 'SelectedSequence' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/SelectedSequence()/selectedSequenceFuncExample.lua
@@ -0,0 +1,11 @@
+--* SelectedSequence() Function - Example
+
+--* About
+    -- This example prints all information about the selected sequence in the Command Line History using the Dump() function.
+
+--* Example 1
+return function ()
+
+    SelectedSequence():Dump()
+
+end
