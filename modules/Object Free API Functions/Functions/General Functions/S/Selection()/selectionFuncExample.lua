13f81a24070e7fdcc6f18191957adb7abaea2a65
Mac Tirney <mactirney@gmail.com>
2024-03-10 17:31:18 -0400

    feat: add Selection Lua function examples
    Establish a 'Selection' function folder to contain Selection Lua function examples.
    Include Selection Lua function examples in the 'Selection' folder.

---
+++ modules/Object Free API Functions/Functions/General Functions/S/Selection()/selectionFuncExample.lua
@@ -0,0 +1,13 @@
+--* Selection() Function - Example
+
+--* About
+    -- This example prints the information of the Selection object in the Command Line History using the Dump() function.
+
+--* Example 1
+local function main()
+
+    Selection():Dump()
+
+end
+  
+return main
